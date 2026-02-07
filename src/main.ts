b479d1509900ce6ecd558b5c7b96fe87338fb1e1
NguyenDThai <thainguyen4646@gmail.com>
2026-02-07 13:10:59 +0700

    open cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://care-dev.vercel.app'],
     credentials: true,
   });
   app.use(cookieParser());
